137fe4c521303b4109ca204fcb55da5c15694d6d
Adam Vandenberg <flangy@gmail.com>
2011-09-30 15:10:38 -0700

    tabs-versus-spaces

--- local.el
+++ local.el
@@ -3,3 +3,7 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 
 (add-to-list 'default-frame-alist '(font . "DejaVu LGC Sans Mono-12"))
+
+;; Don't auto-indent with tabs by default
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
