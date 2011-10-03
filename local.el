afabcc54c1e5a26033e1d85535677f1314096d92
Adam Vandenberg <flangy@gmail.com>
2011-10-03 14:40:34 -0700

    bump down font size

--- local.el
+++ local.el
@@ -2,7 +2,7 @@
 
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 
-(add-to-list 'default-frame-alist '(font . "DejaVu LGC Sans Mono-12"))
+(add-to-list 'default-frame-alist '(font . "DejaVu LGC Sans Mono-10"))
 
 ;; Don't auto-indent with tabs by default
 (setq-default tab-width 4)
