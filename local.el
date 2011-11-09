847cf54bff64b976549e685325799b292d7e7d10
Adam Vandenberg <flangy@gmail.com>
2011-11-09 12:42:24 -0800

    Add comments

--- local.el
+++ local.el
@@ -1,10 +1,13 @@
 (setq inhibit-startup-message t)
 
 ;; UI widgets
+;; No toolbar
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+;; Show menu in GUI mode, but not terminal mode
 (unless window-system
   (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)))
 
+;; Fonts
 (add-to-list 'default-frame-alist '(font . "DejaVu LGC Sans Mono-10"))
 
 ;; Don't auto-indent with tabs by default
