652a214928b0bdf102778600102943a384b35f41
Adam Vandenberg <vandenbe@amazon.com>
2013-08-30 17:08:07 -0700

    F3 in minibuf inserts active filename

--- local.el
+++ local.el
@@ -66,6 +66,19 @@
 (global-set-key (kbd "C-S-k") 'delete-region)
 (global-set-key [C-tab] 'other-window)
 
+; f3 in minibuf inserts active buffer's filename
+; http://stackoverflow.com/questions/455345/in-emacs-how-to-insert-file-name-in-shell-command
+(define-key minibuffer-local-map
+  [f3] (lambda () (interactive)
+       (insert (buffer-name (current-buffer-not-mini)))))
+
+(defun current-buffer-not-mini ()
+  "Return current-buffer if current buffer is not the *mini-buffer*
+  else return buffer before minibuf is activated."
+  (if (not (window-minibuffer-p)) (current-buffer)
+      (if (eq (get-lru-window) (next-window))
+          (window-buffer (previous-window)) (window-buffer (next-window)))))
+
 
 ;; Color themes
 ; Zenburn
