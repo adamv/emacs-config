f991473f418358929fd24c489509d18cdb1fdbff
Adam Vandenberg <vandenbe@amazon.com>
2013-09-10 14:43:45 -0700

    simplify autosave/backup

--- init.el
+++ init.el
@@ -2,27 +2,21 @@
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
-;; custom place to save customizations
-;; See: http://www.gnu.org/s/libtool/manual/emacs/Saving-Customizations.html
-(setq custom-file "~/.emacs.d/custom.el")
-(when (file-exists-p custom-file) (load custom-file))
-
-
 (load "config/ui")
 (load "config/keys")
 (load "config/editing")
 (load "config/modes")
 
 
+;; custom place to save customizations
+;; See: http://www.gnu.org/s/libtool/manual/emacs/Saving-Customizations.html
+(setq custom-file "~/.emacs.d/custom.el")
+(when (file-exists-p custom-file) (load custom-file))
+
+
 ;; don't clutter the workspace with a bunch of backups
-(defvar user-temporary-file-directory
-  (concat temporary-file-directory user-login-name "/"))
-(make-directory user-temporary-file-directory t)
-(setq backup-by-copying t)
+;; store all backup and autosave files in the tmp dir
 (setq backup-directory-alist
-      `(("." . ,user-temporary-file-directory)))
-(setq auto-save-list-file-prefix
-      (concat user-temporary-file-directory ".auto-saves-"))
+      `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
-      `((".*" ,user-temporary-file-directory t)))
-(setq auto-save-default nil)
+      `((".*" ,temporary-file-directory t)))
