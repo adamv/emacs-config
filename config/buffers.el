d61072356c3608beec09ff1ff1986f0f94582cc0
Adam Vandenberg <vandenbe@amazon.com>
2013-09-10 15:30:22 -0700

    ido

--- config/buffers.el
+++ config/buffers.el
@@ -1,8 +1,16 @@
-; autosave to tmp
+;; autosave to tmp
 (setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
-; no backups
+;; no backups
 (setq backup-inhibited t)
 
-; don't confirm opening non-existant files/buffers
+;; don't confirm opening non-existant files/buffers
 (setq confirm-nonexistent-file-or-buffer nil)
+
+;; Interactively Do Things
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t) ; case insensitive matching
+(add-to-list 'ido-ignore-files "\\.DS_Store")
+(setq ido-create-new-buffer 'always) ; always create a new buffer with Ido
+(setq ido-use-virtual-buffers t)
