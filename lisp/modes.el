03387c497feb8dff433fc071049ce31891994c96
Adam Vandenberg <flangy@gmail.com>
2016-07-20 21:35:42 -0700

    enable acme

--- lisp/modes.el
+++ lisp/modes.el
@@ -3,3 +3,8 @@
 ; TextMate
 (require 'textmate)
 (textmate-mode)
+
+(autoload 'acme-mode "acme-mode"
+    "Major mode for editing Acme 6502 sources." t)
+
+(add-to-list 'auto-mode-alist '(".acme$" . acme-mode))
